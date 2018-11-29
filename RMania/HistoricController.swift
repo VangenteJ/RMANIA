7eac5b028f9acb9b19763ebaabaafbd12016e689
DeavyJ <joelvangente--4@hotmail.com>
2018-11-29 21:42:07 +0000

    Image changes

--- RMania/HistoricController.swift
+++ RMania/HistoricController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class HistoricController: UIViewController {
+class HistoricController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,4 +23,47 @@ class HistoricController: UIViewController {
     }
     
 
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+//        if imageNun == 1{
+//            imageview1.image = image
+//        }else if imageNun == 2{
+//            imageview2.image = image
+//        }else if imageNun == 3{
+//            imageview3.image = image
+//        }else if imageNun == 4{
+//            imageview4.image = image
+//        }
+//        imageNun = 0
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func addImage(){
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose source", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
+            
+            if UIImagePickerController.isSourceTypeAvailable(.camera){
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            }
+            
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action:UIAlertAction) in
+            
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+            
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
 }
