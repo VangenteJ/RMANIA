36e19478bd72e928010763016919e7bbadb605fe
DeavyJ <joelvangente--4@hotmail.com>
2018-11-29 09:46:40 +0000

    Initial Commit

---
+++ RMania/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RMania
+//
+//  Created by Joel Vangente on 28/11/2018.
+//  Copyright © 2018 Joel Vangente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
