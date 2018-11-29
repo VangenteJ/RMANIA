0c762758ef650d3ac72177dcbbb193f6a76a904b
DeavyJ <joelvangente--4@hotmail.com>
2018-11-29 16:07:11 +0000

    Add photo functionality and linked screens through self.storyboard.?

--- RMania/HistoricController.swift
+++ RMania/HistoricController.swift
@@ -17,14 +17,9 @@ class HistoricController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func Back(_ sender: Any) {
+        let goBack = self.storyboard?.instantiateViewController(withIdentifier: "MenuController") as! MenuController
+        self.present(goBack, animated: true, completion: nil)
     }
-    */
 
 }
--- RMania/MenuController.swift
+++ RMania/MenuController.swift
@@ -8,15 +8,56 @@
 
 import UIKit
 
-class MenuController: UIViewController {
-
+class MenuController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageview1: UIImageView!
+    @IBOutlet weak var btnToImageView1: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func Winners(_ sender: Any) {
+        let goTo_Winners = self.storyboard?.instantiateViewController(withIdentifier: "HistoricController") as! HistoricController
+        self.present(goTo_Winners, animated: true, completion: nil)
+    }
+    
+    @IBAction func btnAddImage1(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose source", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
+            
+            if UIImagePickerController.isSourceTypeAvailable(.camera){
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            }
+            
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action:UIAlertAction) in
+            
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+            
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        imageview1.image = image
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
